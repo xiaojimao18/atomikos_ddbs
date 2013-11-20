3035537400f714bdd31a4939f7acb57204abb6d8
Nereus <zzrabs@gmail.com>
2013-11-20 12:15:31 +0800

    Signed-off-by: Nereus <zzrabs@gmail.com>

---
+++ src/com/len/trans/controller/LocationController.java
@@ -0,0 +1,12 @@
+package com.len.trans.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LocationController {
+	@RequestMapping(value="/locateSH")
+	public void locateSH() {
+		
+	}
+}
